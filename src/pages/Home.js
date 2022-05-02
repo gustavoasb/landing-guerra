f400ee19f1980fb2892a74255e08765700c72ece
Gustavo Barros <gustavoasb@gmail.com>
2022-05-02 05:36:06 -0300

    feat: add header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
+import Header from "../../src/components/Header"
+
 function App() {
   return (
-    <>    
+    <>
+    {/* Header */}
+      <Header />
+    {/* Partners */}
+    {/* Passo a Passo */}
+    {/* CTA / SIMULAÇãO */}
+    {/* Dados */}
+    {/* Contato */}
+    
     </>
   );
 }
