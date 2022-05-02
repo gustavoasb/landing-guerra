6d87336d827d57a7cbe06b6d0a940c997d8a6874
Gustavo Barros <gustavoasb@gmail.com>
2022-05-02 07:51:54 -0300

    feat: change contact message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl mb-4 lg:mb-0">
-          <span className="block">Fale com nossa equipe</span>
+          <span className="block">Ligamos para você</span>
         </h2>
         <form className="flex flex-row flex-wrap gap-2 w-3/2 bg-blue-100 p-2 shadow">
             <input className="px-2 py-1 w-full outline-blue-100" placeholder="Nome"/>
